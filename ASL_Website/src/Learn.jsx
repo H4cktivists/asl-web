ddc6a1accd47efda5c6804146851ad945a86296c
John Charles J T <111227047+John-C7@users.noreply.github.com>
2024-02-02 16:19:01 +0530

    Hand-Gesture-Capturing
    Signed-off-by: John Charles J T <111227047+John-C7@users.noreply.github.com>

--- ASL_Website/src/Learn.jsx
+++ ASL_Website/src/Learn.jsx
@@ -1,10 +1,11 @@
 import React from "react";
+import Gesture from "./Gesture"; 
 
 export default function Learn(props) {
-  
   return (
-    <div onClick={() => handleClick()}>
-      Learn {sessionStorage.getItem("currLetter")}
+    <div>
+      <div onClick={() => handleClick()}>Learn {sessionStorage.getItem("currLetter")}</div>
+      <Gesture />
     </div>
   );
 }
