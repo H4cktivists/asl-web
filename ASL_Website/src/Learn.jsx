c82bf5503c6759e147c32a28fedf48c77bf4a6f0
ThermalDust095 <79693586+ThermalDust095@users.noreply.github.com>
2024-02-02 22:43:37 +0530

    added learning page

--- ASL_Website/src/Learn.jsx
+++ ASL_Website/src/Learn.jsx
@@ -1,11 +1,32 @@
 import React from "react";
-import Gesture from "./Gesture"; 
+import Gesture from "./Gesture";
+import "./Learn.css";
 
-export default function Learn(props) {
+export default function Learn() {
   return (
-    <div>
-      <div onClick={() => handleClick()}>Learn {sessionStorage.getItem("currLetter")}</div>
-      <Gesture />
+    <div id="lng-main">
+      <div id="lng-bg">
+        <div id="lng-back-btn"></div>
+        <div id="step1" className="lng-steps">
+          <div id="step1-img"></div>
+        </div>
+        <div id="step2" className="lng-steps">
+          <div id="step2-img"></div>
+        </div>
+        <div id="step3" className="lng-steps">
+          <div id="step3-img"></div>
+        </div>
+        <div id="next-img"></div>
+      </div>
+
+      <div id="lng-webcam">
+        <div id="lng-webcam-bg">
+          <h1>{sessionStorage.getItem("currLetter")}</h1>
+        </div>
+      </div>
+      {/* <Gesture /> */}
+
+      {/* <div id="lng-webcam-bg"></div> */}
     </div>
   );
 }
